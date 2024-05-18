055f80feb2046cc1e80f39a7d09fd95f36e4f4ba
Ammar Qammaz <ammarkov@gmail.com>
2024-05-18 12:40:32 +0300

    add some initial files..

---
+++ example.c
@@ -0,0 +1,20 @@
+/** @file example.c
+ *  @brief  An example to organize a large number of concurrently working threads without
+ *  too many lines of code or difficulty understanding what is happening
+ *  https://github.com/AmmarkoV/SharedMemoryVideoBuffers
+ *  @author Ammar Qammaz (AmmarkoV)
+ */
+
+//Can also be compiled using :
+//gcc  -O3 example.c -pthread -lm -o example
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+   
+}
