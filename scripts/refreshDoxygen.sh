3e96e7c294b86ff675c84140ae43d4e301934dc0
Ammar Qammaz <ammarkov@gmail.com>
2026-09-15 16:05:47 +0300

    ...

--- scripts/refreshDoxygen.sh
+++ scripts/refreshDoxygen.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
+# Generates the documentation: doc/html/, doc/latex/ and SharedMemoryVideoBuffers.pdf in the project root.
+# Needs doxygen, graphviz (dot) and pdflatex.
 
+STARTDIR=`pwd`
 #Switch to this directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR"
- 
-export DOXYGEN_INPUT="$DIR/.."
-export DOXYGEN_OUTPUT="$DIR/../doc"
+cd "$DIR/.."
+ROOT=`pwd`
 
-cd ..
-doxygen doc/doxyfile
-cd doc/latex
-make
-cd .. 
-cd ..
+# Read by doc/doxyfile
+export DOXYGEN_INPUT="$ROOT"
+export DOXYGEN_OUTPUT="$ROOT/doc"
 
-ln -s doc/latex/refman.pdf SharedMemoryVideoBuffers.pdf
+# Start clean, so documentation of removed files doesn't linger
+rm -rf doc/html doc/latex
 
+if ! doxygen doc/doxyfile; then
+  echo "doxygen failed"
+  cd "$STARTDIR"
+  exit 1
+fi
+
+if ! make -C doc/latex; then
+  echo "Building the PDF failed, see doc/latex/refman.log"
+  cd "$STARTDIR"
+  exit 1
+fi
+
+rm -f SharedMemoryVideoBuffers.pdf
+cp doc/latex/refman.pdf SharedMemoryVideoBuffers.pdf
+echo "Generated $ROOT/SharedMemoryVideoBuffers.pdf and $ROOT/doc/html/index.html"
 
 cd "$STARTDIR"
- 
 
 exit 0
