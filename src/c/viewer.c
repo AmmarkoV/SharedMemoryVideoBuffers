4a6cc79cfe12bb7016f63e2b21f44be93ed70880
Ammar Qammaz <ammarkov@gmail.com>
2026-09-15 10:34:26 +0300

    fix warnings..

--- src/c/viewer.c
+++ src/c/viewer.c
@@ -75,7 +75,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    struct VideoFrameLocalMapping localMap={0};
+    //struct VideoFrameLocalMapping localMap={0};
     if (map_frame_shared_memory(frame,1) == NULL)  //We want to overwrite the frame->data because we are the client and this makes the python API easier
     {
         return EXIT_FAILURE;
